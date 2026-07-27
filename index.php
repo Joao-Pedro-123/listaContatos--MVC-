8b30ff2a74e1f46bb37a64e1bd3af94ed40de35c
Joao-Pedro-123 <joao.almeida332@etec.sp.gov.br>
2026-07-27 19:10:01 -0300

    correção de pequeno bug visual

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         for ($i = 0; $i < $rows ; $i++) {  //Como o resultado começa em 0, o $i só chegará até o máximo de registros possíveis de serem buscados. Ex: O $rows retorna 8, os resultados começam com um index igual a 0, então teremos apenas 7 registros. Logo, o $i precisa ser menor que o $rows. E é bom utilizar uma variável ao invés de um número fixo ali pois agiliza a busca de registro, não necessitando a visualização do numero de colunas do registro toda vez que mudar a consulta.
             if ($dados[$i] != ""){
                 if ($i == ($rows -1)){ //Quando o $i for igual ao número de $rows -1(ja que os resultados começam no 0) então quer dizer que é o ultimo resultado da busca.
-                    echo $dados[$i], $i, '<br> <br>'; //Caso a condição seja verdadeira, aqui são adicionadas duas quebras de linha.
+                    echo $dados[$i], '<br> <br>'; //Caso a condição seja verdadeira, aqui são adicionadas duas quebras de linha.
                 } else{
                     echo $dados[$i], '<br>';//Caso não, apenas uma quebra de linha é adicionada.
                 }
