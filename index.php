89c1b6050ca6d4b7ae432727d9f2aa6e0051326c
PauloV-142 <moraisdealmeidapaulovictor@gmail.com>
2026-08-03 23:30:54 -0300

    Try: Talvez Joins para pegar os dados

--- index.php
+++ index.php
@@ -4,12 +4,27 @@ require_once('model.php');
 
 <h1>Insert (Create)</h1>
 <?php
-    create("email", [1, 'paulo-victor@gut', 'envie apenas documentos .ods']);
+    // create("email", [1, 'paulo-victor@gut', 'envie apenas documentos .ods']);
 ?>
 
 <h1>Select (Read)</h1>
 <?php 
-    read('*', ['contato', 'email']); 
+    // read('*', ['contato', 'email']); 
+    read('*', 'contato');
+    /*
+SELECT * FROM `contato` as `c`
+INNER JOIN `email` as `em`
+ON `c`.`id_contato` = `em`.`id_contato`
+
+INNER JOIN `endereco` as `en`
+ON `c`.`id_contato` = `en`.`id_contato`
+
+INNER JOIN `rede_social` as `rede`
+ON `c`.`id_contato` = `rede`.`id_contato`
+
+INNER JOIN `telefone` as `tel`
+ON `c`.`id_contato` = `tel`.`id_contato`;
+    */
 ?>
 
 <h1>Update</h1>
